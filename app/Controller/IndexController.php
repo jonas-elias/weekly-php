72e0bcd3f5f92fee31904ea5906190a2d21aa556
jonas-elias <jonasdasilvaelias@gmail.com>
2024-09-09 02:17:48 -0300

    add: semantic-search method

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -12,16 +12,42 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Config\ConfigQdrant;
+use Codewithkyrian\Transformers\Transformers;
+use Hyperf\Qdrant\Api\Points;
+use Hyperf\Qdrant\Connection\HttpClient;
+use Hyperf\Qdrant\Struct\Points\VectorStruct;
+use Hyperf\Qdrant\Struct\Points\WithPayload;
+
+use function Codewithkyrian\Transformers\Pipelines\pipeline;
+
 class IndexController extends AbstractController
 {
-    public function index()
+    public function search()
     {
-        $user = $this->request->input('user', 'Hyperf');
-        $method = $this->request->getMethod();
+        $client = new HttpClient(new ConfigQdrant());
+        $points = new Points($client);
 
-        return [
-            'method' => $method,
-            'message' => "Hello {$user}.",
-        ];
+        Transformers::setup()
+            ->apply();
+        $extractor = pipeline('embeddings', 'Xenova/all-MiniLM-L6-v2');
+
+        $input = $this->request->query('q');
+        if (! $input) {
+            return $this->response->json([
+                'error' => 'filter q not found'
+            ])->withStatus(400);
+        }
+
+        $embedding = $extractor($input, normalize: true, pooling: 'mean');
+
+        $response = $points->searchPoints(
+            'collection',
+            new VectorStruct($embedding[0]),
+            3,
+            withPayload: new WithPayload(true),
+        );
+
+        return $this->response->json($response);
     }
 }
